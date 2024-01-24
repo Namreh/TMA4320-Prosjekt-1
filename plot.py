fbac57c0c87f4d7b88354a56a86cc8ba89b6f81a
Namreh <herman@neple.no>
2024-01-24 15:54:19 +0100

    oppgave 2e i plot

--- plot.py
+++ plot.py
@@ -14,4 +14,22 @@ def allowedRotOfN(N_s, start, stop):
     plt.show()
 
 #kjør denne for å vise oppg 1i
-allowedRotOfN(100, 10, 1000)
+#allowedRotOfN(100, 10, 1000)
+    
+#Oppgave 2e
+def oppgave2E():
+    N = 30
+    N_s = 5000
+    t_array = np.arange(10,1000, 30)
+    length = len(t_array)
+    e_array = np.zeros(length)
+    deviation_array = np.zeros(length)
+
+    for i in range(length):
+        _, energy_array = m.metropolisSimulation(m.initalPolymer(N), N_s, m.createVarray(N, -4.0*10**(-21)), t_array[i])
+        e_array[i] = np.mean(energy_array[1000:])
+        deviation_array[i] = np.std(energy_array[1000:])
+    
+    fig, ax = plt.subplots()
+    ax.errorbar(t_array, e_array, yerr=deviation_array)
+    plt.show()
